5ee264711bf43b0f1769656e05757e04d194e3d0
tintun123 <mr.tintunoo1997@gmail.com>
2022-07-16 19:04:36 +0630

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoverComponent } from './cover/cover.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { SorryComponent } from './sorry/sorry.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoverComponent,
+    NavbarComponent,
+    CardListComponent,
+    SorryComponent,
+    HomeComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  
+  faplus = faPlus;
+  faminus = faMinus;
+  toggl : Boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() : void {
+    this.toggl = !this.toggl
+  }
+
+}
